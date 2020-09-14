bab17af368ab098fe37c4e72c908cc3615c95967
krisztianbugjo <b.chris95@hotmail.com>
2020-09-14 14:45:52 +0200

    Modify SearchFragment Add Debouncing

--- app/src/main/java/com/example/task1/UI/SearchFragment.kt
+++ app/src/main/java/com/example/task1/UI/SearchFragment.kt
@@ -1,9 +1,12 @@
 package com.example.task1.UI
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -11,6 +14,9 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.task1.Adapter.MovieAdapter
 import com.example.task1.Model.Movie
 import com.example.task1.R
+import kotlinx.android.synthetic.main.fragment_search.search_text
+import java.util.Timer
+import java.util.TimerTask
 
 class FirstFragment : Fragment() {
     lateinit var movies: ArrayList<Movie>
@@ -18,7 +24,7 @@ class FirstFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View = inflater.inflate(R.layout.fragment_first, container, false)
+    ): View = inflater.inflate(R.layout.fragment_search, container, false)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -27,6 +33,7 @@ class FirstFragment : Fragment() {
         val adapter = MovieAdapter(movies) { movie -> changeFragment(movie) }
         rvContacts.adapter = adapter
         rvContacts.layoutManager = LinearLayoutManager(requireContext())
+        initSearchText()
     }
 
     fun changeFragment(movie: Movie) {
@@ -37,6 +44,32 @@ class FirstFragment : Fragment() {
         val bundle = bundleOf("title" to movie.title)
         fragment.arguments = bundle
         transaction.commit()
+    }
+
+    fun initSearchText() {
+        search_text.addTextChangedListener(
+            object : TextWatcher {
+                override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
+                override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+                private var timer: Timer = Timer()
+                private val DELAY: Long = 500 // milliseconds
+                override fun afterTextChanged(s: Editable) {
+                    timer.cancel()
+                    timer = Timer()
+                    timer.schedule(
+                        object : TimerTask() {
+                            override fun run() {
+                                requireActivity().runOnUiThread(java.lang.Runnable {
+                                    Toast.makeText(context, search_text.text, Toast.LENGTH_SHORT)
+                                        .show()
+                                })
+                            }
+                        },
+                        DELAY
+                    )
+                }
+            }
+        )
     }
 
     private fun createMovieList(numMovies: Int): ArrayList<Movie> {
